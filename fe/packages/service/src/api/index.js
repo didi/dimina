9efab90ce753043ff9c6e91c126b0244981b4fc0
dos1in <doslino@gmail.com>
2025-06-09 20:23:08 +0800

    refactor(fe,service): simplify API handling by removing proxy and unused code

--- fe/packages/service/src/api/index.js
+++ fe/packages/service/src/api/index.js
@@ -1,22 +1,8 @@
 const apiInfo = import.meta.glob('./core/**/index.js', { eager: true })
-const api = {}
+const globalApi = {}
 for (const f of Object.values(apiInfo)) {
 	for (const [k, v] of Object.entries(f)) {
-		api[k] = v
+		globalApi[k] = v
 	}
 }
-const handler = {
-	get(target, prop, receiver) {
-		const origMethod = Reflect.get(target, prop, receiver)
-
-		return origMethod
-	},
-}
-/**
- * 外部挂载 API，内部转发不存在 API
- * [Render]invokeAPI -> [Container]invokeAPI -> [Service]invokeAPI -> [Container]invokeAPI
- */
-
-const globalApi = new Proxy(api, handler)
-
 export default globalApi
